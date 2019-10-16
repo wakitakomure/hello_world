c8f69512e183cda538e9ec50891fe5d9309c885d
wonderfulcode <wonderfulcode2019@gmail.com>
2019-10-16 15:51:04 +0900

    ログイン・ログアウトの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get '/', to: 'pages#index'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :users
 end
